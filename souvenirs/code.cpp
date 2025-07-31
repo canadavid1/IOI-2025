60e65f6b9712dce761e103d56bd358ce3fccb720
canadavid1 <davideikeland005@gmail.com>
2025-07-31 18:52:31 +0200

    souvenirs st5

--- souvenirs/code.cpp
+++ souvenirs/code.cpp
@@ -1,5 +1,6 @@
 #include "souvenirs.h"
 #include <iostream>
+#include <tuple>
 /*
     have to query > P0-N for first
     
@@ -33,31 +34,79 @@
 
 
     not adaptive (!)
+
+    st5:
+        P[i] >= P[i+1] + P[i+2] 
+        P[i] <= 2P[i+1]
+
+        2P[i+1] >= P[i] >= P[i+1] + P[i+2]
+
+        P[i] > 2P[i+2] >= P[i+1]
+
+        P[i+1] >= P[i]-P[i+1] >= P[i+2]
+
+        how does this help?
+        
+        from bottom: P[i+2] => query 2P[i+2]
+        this guarantees to get P[i+1] but not P[i]
+
+        again, need to get Plast
+
+        halve each time? yes
+        P[i]/2 >= P[i-1]
 */
 
 void buy_souvenirs(int N, long long P0) {
-    // st3
     std::vector<long long> P(N,0);
     std::vector<int> ct(N,0);
-
+    std::vector<std::tuple<std::vector<int>,long long,long long>> hist;
+    hist.reserve(5000);
     P[0] = P0;
-    auto test = [&](long long M){
+    auto test = [&](long long M) -> std::pair<std::vector<int>&,long long> {
         auto[o,r] = transaction(M);
         for(auto i : o) ct[i]++;
-        return std::pair{o,r};
+        hist.emplace_back(std::move(o),r,M);
+        return {std::get<0>(hist.back()),r};
     };
+    int Pt = P[0]>>((N-1)/2);
     for(int i = 1; i < N; i++)
     {
-        if (P[i]) continue;
-        auto[o,r] = test(P[i-1]-1);
-        if (o.size() > 1 || r == 1) {
-            P[i] = P[i-1]-2;
-            if (o.size() > 1) {P[N-1] = 1;}
-        }
-        else {
-            P[i] = P[i-1]-1;
+        Pt = Pt/2;
+        // if (P[i]) continue;
+        auto[o,r] = test(Pt);
+        std::cerr << "query " << Pt << " to get r=" << r << " and o =";
+        for(auto e : o) std::cerr << " " << e;
+        std::cerr << "\n";
+        if (o.size() == 1) {
+            P[o[0]] = Pt-r;
+            if (o[0] == N-1) break;
+            if (o[0] == N-2) {
+                Pt = 2*(P[N-2]-1);
+            }
         }
     }
+    std::cerr << "here\n";
+    int hi = hist.size()-1;
+    for(int i = N-1; i >= 1; i--) {
+        if (P[i]) continue;
+        long long s = 0;
+        while (hi >= 0 && std::get<0>(hist[hi]).front() > i) hi--;
+        std::vector<int> o;
+        long long r,M;
+        if (hi >= 0) std::tie(o,r,M) = hist[hi];
+        if (hi < 0 || o[0] != i) {
+            std::tie(o,r) = test(P[i+1]*2);
+            M = P[i+1]*2;
+        }
+        bool contains = 0;
+        for(auto j : o) {
+            s += P[j];
+        }
+        P[i] = M - r - s;
+    }
+    std::cerr << "P[i] =";
+    for(auto i : P) std::cerr << " " << i;
+    std::cerr << "\n";
     
 
     for(int i = 1; i < N; i++)
--- souvenirs/gen.py
+++ souvenirs/gen.py
@@ -1,8 +1,14 @@
+import sys
 import random
-N=10
+N=50
 P = [1]
-for i in range(N):
-    P.append(P[-1]+2)
+P.append(random.randint(P[-1]+1,P[-1]*2))
+for i in range(2,N):
+    P.append(random.randint(P[-1]+P[-2],max(int(P[-1]*1.1),P[-1]+P[-2]+1)))
 P.reverse()
+print(*P,file=sys.stderr)
+if P[0] >= 10**15:
+    print("bad generator",file=sys.stderr)
+    exit(1)
 print(N)
 print(*P)
