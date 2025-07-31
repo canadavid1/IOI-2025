193b4774fe0174d78fa53e00f12475015bee99d5
canadavid1 <davideikeland005@gmail.com>
2025-07-31 16:22:14 +0200

    souvenirs st4

--- souvenirs/code.cpp
+++ souvenirs/code.cpp
@@ -1,10 +1,35 @@
 #include "souvenirs.h"
+/*
+    have to query > P0-N for first
+    
 
+    N=3:
+        query p-1
+            only 1:
+                know price, ans that-1 twice
+            both:
+                know sum, ans half (round down)
+*/
 
 void buy_souvenirs(int N, long long P0) {
-    // st2
-    for(int i = 1; i < N; i++) {
-        auto Pi = P0 - i;
-        for(int j = 0; j < i; j++) transaction(Pi); 
+    std::vector<long long> P(N,-1);
+    std::vector<int> ct(N,0);
+
+    P[0] = P0;
+    // auto test = [&](long long M){
+    //     auto[o,r] = transaction(M);
+    //     for(auto i : o) ct[i]++;
+    //     return std::pair{o,r};
+    // };
+    auto[o,r] = transaction(P0-1);
+    if (o.size() == 1) {
+        int P1 = P0-1-r;
+        transaction(P1-1);
+        transaction(P1-1);
     }
+    else {
+        int P12 = P0-1-r;
+        transaction(P12/2);
+    }
+    
 }
