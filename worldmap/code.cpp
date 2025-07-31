81b8ef49c732fc3d008de5cd473a9855d60dd48a
canadavid1 <davideikeland005@gmail.com>
2025-07-31 16:41:56 +0200

    worldmap st1

--- worldmap/code.cpp
+++ worldmap/code.cpp
@@ -1,18 +1,11 @@
 
 #include "worldmap.h"
 std::vector<std::vector<int>> create_map(int N, int M,std::vector<int> A, std::vector<int> B) {
-    // st4
-    const int K = 240;
-    std::vector<std::vector<int>> o(K,std::vector<int>(K,1));
-    int x = 0;
-    int y = 0;
-    for(int i = 0; i < M; i++) {
-        o[y][x++] = A[i];
-        o[y][x++] = B[i];
-        if(++x == K) {
-            x = 0;
-            y += 2;
-        }
+    // st34
+    const int K = N;
+    std::vector<std::vector<int>> o(N,std::vector<int>(N,0));
+    for(int i = 0; i < K; i++) {
+        o[i] = std::vector<int>(N,i+1);
     }
     return o;
 }
---
+++ worldmap/gen.py
@@ -0,0 +1,25 @@
+import random
+
+N = 40
+M = N-1
+p = list(range(1,N+1))
+random.shuffle(p)
+e: set[tuple[int,int]] = set()
+for i in range(1,N):
+    e.add(tuple(sorted((random.choice(p[:i]),p[i]))))
+
+assert M <= (N*(N-1))//2
+if M >= (N*(N-1))//4:
+    ed = [(i,j) for i in range(N) for j in range(i+1,N+1)]
+    random.shuffle(ed)
+    while len(e) < M:
+        e.add(ed.pop())
+else:
+    while len(e) < M:
+        e.add(tuple(sorted((random.randint(1,N),random.randint(1,N)))))
+
+print(N,M)
+# for x,y in e:
+#     print(x,y)
+for i in range(1,N):
+    print(i,i+1 )
