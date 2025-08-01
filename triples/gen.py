fd9ffdb62924459a5e1fa68be70958024da0360c
canadavid1 <davideikeland005@gmail.com>
2025-08-01 06:42:27 +0200

    triples II st1: 22

--- triples/gen.py
+++ triples/gen.py
@@ -1,7 +1,18 @@
 
+def int_or(a):
+    try:
+        return int(a)
+    except:
+        return None
+
 import random
+f = lambda i: (i-[0,2,10][(i+1)%3])
 N = 20
-H = [max(1,i) for i in range(N)]
+H = list(map(int_or,"3 1 3 - - 5 4 - - -  3".split()))
+H += [None]*(N-len(H))
+for i in range(N):
+    if H[i] == None: H[i] = f(i)
+# H = [max(1,i) for i in range(N)]
 print(1)
 print(N)
 print(*H)
