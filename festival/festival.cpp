7c1f57208fee27d616759a741c4a69791779a432
canadavid1 <davideikeland005@gmail.com>
2025-08-03 11:44:33 +0200

    festival st1

--- festival/festival.cpp
+++ festival/festival.cpp
@@ -1,5 +1,16 @@
 #include "festival.h"
+#include <algorithm>
+
 
 std::vector<int> max_coupons(int A, std::vector<int> P, std::vector<int> T) {
-  return {};
+    std::vector<std::pair<int,int>> coupons;
+    for(int i = 0; i < P.size(); i++) coupons.emplace_back(P[i],i);
+    std::sort(coupons.begin(),coupons.end());
+    std::vector<int> o;
+    for(auto[v,i] : coupons) {
+        if (v > A) break;
+        o.push_back(i);
+        A -= v;
+    }
+    return o;
 }
