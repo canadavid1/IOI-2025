e835b62796f625022a420584d6d3c01dad28bbc0
canadavid1 <davideikeland005@gmail.com>
2025-08-03 14:43:02 +0200

    festival st3

--- festival/festival.cpp
+++ festival/festival.cpp
@@ -1,7 +1,8 @@
 #include "festival.h"
 #include <algorithm>
 #include <set>
-
+#include <map>
+#include <iostream>
 std::vector<int> got_infinity(std::vector<int> o,int N) {
     // if X >= N*max(P) then can do all rest
     std::set<int> seen;
@@ -16,27 +17,37 @@ std::vector<int> max_coupons(int A, std::vector<int> P, std::vector<int> T) {
     std::vector<std::pair<int,int>> coupons[5];
     for(int i = 0; i < N; i++) coupons[T[i]].emplace_back(P[i],i);
     for(auto& i : coupons) std::sort(i.begin(),i.end());
+    std::map<long long,int> num_T1_coupons;
+    long long s = 0;
+    for(int i = 0; i < coupons[1].size(); i++) {
+        num_T1_coupons[s] = i;
+        s += coupons[1][i].first;
+    }
+    num_T1_coupons[s] = coupons[1].size();
+    // for(auto[k,v]: num_T1_coupons) std::cerr << k << "->" << v << "\n";
+
     // buy all bought coupons with T=2 before T=1
-    std::vector<int> best;
+    // num total, num T2
+    std::pair<int,int> best = {0,0};
+    long long X = A;
+    std::vector<int> o;
     for(int T2 = 0; T2 <= coupons[2].size(); T2++) {
-        long long X = A;
-        std::vector<int> o;
         // buy T2 coupons of T=2
-        for(int i = 0; i < T2; i++) {
-            auto[v,idx] = coupons[2][i];
+        if (T2 > 0) {
+            auto[v,i] = coupons[2][T2-1];
             if (X < v) break;
-            o.push_back(idx);
+            o.push_back(i);
             X -= v;
             X *= 2;
-            if (X >= N * 1000000) return got_infinity(o,N);
+            if (X > N*1000*1000*100) return got_infinity(o,N);
         }
-        for(auto[v,idx] : coupons[1]) {
-            if (X < v) break;
-            o.push_back(idx);
-            X -= v;
-        }
-        if (o.size() > best.size()) best = std::move(o);
+        int T1 = (--num_T1_coupons.upper_bound(X))->second;
+        best = std::max(best,{T1+T2,T2});
     }
-    end:;
-    return best;
+    int T2 = best.second;
+    int T1 = best.first-T2;
+    o.clear();
+    for(int i = 0; i < T2; i++) o.push_back(coupons[2][i].second);
+    for(int i = 0; i < T1; i++) o.push_back(coupons[1][i].second);
+    return o;
 }
