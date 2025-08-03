7ad591aba197cd56b8109b9c41640498c45fed01
canadavid1 <davideikeland005@gmail.com>
2025-08-03 12:37:48 +0200

    migrations st1 Z<9998 (16p)

--- migrations/migrations.cpp
+++ migrations/migrations.cpp
@@ -1,16 +1,20 @@
 #include "migrations.h"
 
-int send_message(int N, int i, int Pi) {
-  if (i == 1)
-    return 10;
-  else if (i == 2)
-    return 20;
-  else if (i == 3)
-    return 30;
-  else
-    return 0;
+
+std::vector<int> P = {-1};
+int send_message(int N, int i, int Pi) { // this can retain information across calls
+    P.push_back(Pi);
+    if (i < N-1) return 0;
+    std::vector<int> d(N);
+    d[0] = 0; 
+    int mi=0;
+    for(int j = 1; j < N; j++) {
+        d[j] = d[P[j]] + 1;
+        if (d[j] > d[mi]) mi = j;
+    }
+    return mi-2;
 }
 
 std::pair<int, int> longest_path(std::vector<int> S) {
-  return {0, 3};
+    return {0, S.back()+2};   
 }
